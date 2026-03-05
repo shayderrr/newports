651acb2b689251fef84ab83454886d6b35dc0ec8
shayderrr <pranavbajjuri@gmail.com>
2026-03-05 06:43:38 -0600

    a

--- 22674/assets/data/resting/video.lua
+++
@@ -1,8 +0,0 @@
-function onEndSong()
-	if not allowEnd and isStoryMode then
-		startVideo('midnightCutscene');
-		allowEnd = true;
-		return Function_Stop;
-	end
-	return Function_Continue;
-end
--- 22674/assets/stages/bed.lua
+++
@@ -1,9 +0,0 @@
-function onCreate()
-	-- background shit
-	makeLuaSprite('bed', 'leroom/bed', -500, -200);
-	setScrollFactor('bed', 0.9, 0.9);
-
-	addLuaSprite('bed', false);
-	
-	close(true); --For performance reasons, close this script once the stage is fully loaded, as this script won't be used anymore after loading the stage
-end
