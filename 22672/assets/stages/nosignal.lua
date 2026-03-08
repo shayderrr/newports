f1f77a9675fa5f728038c44757f4a2ed7495993e
shayderrr <pranavbajjuri@gmail.com>
2026-03-07 19:21:13 -0600

    twin idk

---
+++ 22672/assets/stages/nosignal.lua
@@ -0,0 +1,8 @@
+function onCreate()	
+	makeLuaSprite('nosignal','nosignal',-1300,-520)
+	setScrollFactor('nosignal', 0.9, 0.9);
+	scaleObject('nosignal', 1.4, 1.5);
+
+    addLuaSprite('nosignal', false);
+end
+
