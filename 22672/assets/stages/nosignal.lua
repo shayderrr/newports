511beb7263df6961ab3750f205508b46f7d10cd3
shayderrr <pranavbajjuri@gmail.com>
2026-03-08 09:14:26 -0500

    a

--- 22672/assets/stages/nosignal.lua
+++
@@ -1,8 +0,0 @@
-function onCreate()	
-	makeLuaSprite('nosignal','nosignal',-1300,-520)
-	setScrollFactor('nosignal', 0.9, 0.9);
-	scaleObject('nosignal', 1.4, 1.5);
-
-    addLuaSprite('nosignal', false);
-end
-
